c19fbfdd34c5dcc85c4c034b3fb60ea5bdd19f0f
gomjellie <gomjellie@gmail.com>
2021-01-12 02:24:08 +0900

    카드 선택, 해재 뷰 구현

--- Set/ViewController.swift
+++ Set/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         hideButtons()
         game.deckShuffle()
@@ -25,7 +24,7 @@ class ViewController: UIViewController {
     private func hideButtons() {
         for button in buttons {
             button.layer.borderWidth = 3.0
-            button.layer.borderColor = UIColor.systemPurple.cgColor
+            button.layer.borderColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
             button.layer.cornerRadius = 8.0
             button.isHidden = true
         }
@@ -34,7 +33,7 @@ class ViewController: UIViewController {
     private let shapes = ["▲", "●", "■"]
     private let colors = [#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1), #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)]
     private let numbers = [1, 2, 3]
-    private let shadings = [1 , 0.35, 2]
+    private let shadings = [1 , 0.25, 2]
     
     private func updateViewFromModel() {
         for index in game.fields.indices {
@@ -42,20 +41,32 @@ class ViewController: UIViewController {
             let button = buttons[index]
             let shape = shapes[field.shape.index]
             let color = colors[field.color.index]
-//            let number = numbers[field.number.index]
+            let number = numbers[field.number.index]
             let shading = shadings[field.shading.index]
+            
+            let cardTitle = String(repeating: shape, count: number)
+            
             let attributes: [NSAttributedString.Key: Any] = [
                 .strokeWidth: shading == 2 ? 6 : -1,
                 .foregroundColor: color.withAlphaComponent(CGFloat(shading)),
             ]
-            let title: NSAttributedString = NSAttributedString(string: shape, attributes: attributes)
+            let attributedTitle: NSAttributedString = NSAttributedString(string: cardTitle, attributes: attributes)
             
-            button.setAttributedTitle(title, for: UIControl.State.normal)
+            button.setAttributedTitle(attributedTitle, for: UIControl.State.normal)
             button.setTitleColor(color, for: UIControl.State.normal)
             button.isHidden = false
         }
     }
 
+    @IBAction func onCardClick(_ sender: UIButton) {
+        if let index = buttons.firstIndex(of: sender) {
+//            let card = game.fields[index]
+            let isSelected = sender.layer.borderColor == #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+            buttons[index].layer.borderColor = isSelected ? #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1) : #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+        }
+        
+    }
+    
     @IBOutlet var buttons: [UIButton]!
     
     @IBAction func onClickDealCard(_ sender: UIButton) {
